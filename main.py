6e33f92dbc0835dff02a444ff4745d71b25d3096
Melissa-Andrea <melissa.chiri@sistemas.edu.bo>
2024-11-08 18:28:11 -0400

    visualizing video

---
+++ main.py
@@ -0,0 +1,16 @@
+import cv2
+
+video_path= 'C:/Users/Melissa/Desktop/videos/parking_crop_loop.mp4'
+
+cap = cv2.VideoCapture(video_path)
+
+ret =True
+while ret:
+    ret, frame = cap.read()
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+   
+    
+cap.release()
+cv2.destroyAllWindows()
